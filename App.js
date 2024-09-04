ef452f791953e486886ead2625f249bb46207142
Ankur Halder <ankur.halder12345@gmail.com>
2024-09-04 07:48:50 +0530

    chore: Remove unnecessary code and update text in App.js

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World Ankur Halder!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
     </View>
   );
 }
