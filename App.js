fac0ff16ac8fed471bfe81f46dcb3c5c8b3885c4
Ankur Halder <ankur.halder12345@gmail.com>
2024-09-05 00:52:38 +0530

    chore: Update App.js to improve user input styling

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your Course goal!" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} placeholder="Your Course goal!" />
         <Button title="ADD Goal" />
       </View>
       <View>
@@ -16,5 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  TextInput: {
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    padding: 5,
+    width: "80%",
+    marginRight: 10,
   },
 });
