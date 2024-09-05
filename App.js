66db685804810869a65351fcddd8680859cf067b
Ankur Halder <ankur.halder12345@gmail.com>
2024-09-05 09:47:09 +0530

    Refactor App.js to improve user input styling and layout

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
         <TextInput style={styles.TextInput} placeholder="Your Course goal!" />
         <Button title="ADD Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -15,18 +15,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   TextInput: {
     borderColor: "#cccccc",
     borderWidth: 1,
     padding: 5,
-    width: "80%",
+    width: "70%",
     marginRight: 10,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
