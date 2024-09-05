74e7ba562a4da507323a99e9b8b43d23f5406038
Ankur Halder <ankur.halder12345@gmail.com>
2024-09-05 10:00:38 +0530

    Refactor App.js to improve user input styling and layout

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.TextInput} placeholder="Your Course goal!" />
-        <Button title="ADD Goal" />
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Your Course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
