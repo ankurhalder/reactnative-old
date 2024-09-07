6c6daa7ada3f8c743f46ee9adab75587fcdee66f
Ankur Halder <ankur.halder12345@gmail.com>
2024-09-07 07:31:32 +0530

    Refactor GoalInput component to improve user experience and add close button

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -5,11 +5,18 @@ function GoalInput({ goalInputHandler, addGoalHandler, modalIsVisible }) {
     <Modal visible={modalIsVisible} animationType="slide">
       <View style={styles.inputContainer}>
         <TextInput
-          style={styles.TextInput}
+          style={styles.textInput}
           placeholder="Your Course goal!"
           onChangeText={goalInputHandler}
         />
-        <Button title="Add Goal" onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Close" />
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -19,18 +26,26 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: "column",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
   },
-  TextInput: {
+  textInput: {
     borderColor: "#cccccc",
     borderWidth: 1,
     padding: 5,
-    width: "70%",
-    marginRight: 10,
+    width: "90%",
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
   },
 });
