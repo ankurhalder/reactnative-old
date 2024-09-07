517127ffa3f852af83876d272faade3944a74d19
Ankur Halder <ankur.halder12345@gmail.com>
2024-09-07 07:13:00 +0530

    Refactor GoalItem.js to improve delete functionality and add visual feedback

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem({ itemData, deleteGoalHandler, id }) {
   return (
-    <Pressable onPress={deleteGoalHandler.bind(this, id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#2106" }}
+        onPress={deleteGoalHandler.bind(this, id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{itemData.item.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -15,11 +19,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
+    padding: 8,
     color: "white",
   },
 });
